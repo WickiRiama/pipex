07ccaf0901860578fe857e142b860c8aff6efe3c
Margaux RIANT <mriant@e2r12p2.clusters.42paris.fr>
2022-03-24 18:06:37 +0100

    draft working

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,49 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 16:58:40 by mriant            #+#    #+#             */
-/*   Updated: 2022/03/24 14:27:34 by mriant           ###   ########.fr       */
+/*   Updated: 2022/03/24 18:05:25 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "pipex.h"
 #include <fcntl.h>
+#include <sys/wait.h>
+
+int	ft_do_cmd1(char **cmd, int fd_in, int fd_pipe[2], char **aenv)
+{
+	int	fd_stdin;
+	int	fd_stdout;
+
+	fd_stdin = dup2(fd_in, 0);
+	close(fd_in);
+	fd_stdout = dup2(fd_pipe[1], 1);
+	close(fd_pipe[1]);
+	if (fd_stdin == -1 || fd_stdout == -1)
+		return (1);
+	if (execve(cmd[0], cmd, aenv) == -1)
+		return (1);
+	return (0);
+
+}
+
+int	ft_do_cmd2(char **cmd, int fd_out, int fd_pipe[2], char **aenv)
+{
+	int	fd_stdin;
+	int	fd_stdout;
+
+	fd_stdin = dup2(fd_pipe[0], 0);
+	close(fd_pipe[0]);
+	ft_printf("ici\n");
+	fd_stdout = dup2(fd_out, 1);
+	close(fd_out);
+	if (fd_stdin == -1 || fd_stdout == -1)
+		return (1);
+	if (execve(cmd[0], cmd, aenv) == -1)
+		return (1);
+	return (0);
+
+}
 
 int	main(int ac, char **av, char **aenv)
 {
@@ -20,7 +56,9 @@ int	main(int ac, char **av, char **aenv)
 	char	**cmd2;
 	int		fd_f1;
 	int		fd_f2;
-	int		fd_stdin;
+	int		fd_pipe[2];
+	pid_t	pid_child1;
+	pid_t	pid_child2;
 
 	if (ac != 5)
 	{
@@ -33,17 +71,34 @@ int	main(int ac, char **av, char **aenv)
 		return (1);
 	fd_f1 = open(av[1], O_RDONLY);
 	fd_f2 = open(av[4], O_CREAT | O_TRUNC | O_WRONLY, 00700);
-	if (fd_f1 == -1 || fd_f2 == -1)
+	if (fd_f1 == -1 || fd_f2 == -1 || pipe(fd_pipe) == -1)
 	{
 		ft_fprintf(2, "Error\nCan't open file.\n");
 		ft_clean_array(cmd1);
 		ft_clean_array(cmd2);
 		return (1);
 	}
-	fd_stdin = dup2(fd_f1, 0);
-	ft_printf("%d\n", fd_stdin);
-	execve(cmd1[0], cmd1, aenv); // verifier le retour
+	pid_child1 = fork();
+	if (pid_child1 == 0)
+	{
+		if (ft_do_cmd1(cmd1, fd_f1, fd_pipe, aenv))
+			exit(1);
+		exit(0);
+	}
+	waitpid(pid_child1, 0, 0);
+	close (fd_pipe[1]);
+	pid_child2 = fork();
+	if (pid_child2 == 0)
+	{
+		if (ft_do_cmd2(cmd2, fd_f2, fd_pipe, aenv))
+			exit(1);
+		exit(0);
+	}
+
+	waitpid(pid_child2, 0, 0);
+	close (fd_pipe[0]);
 	ft_clean_array(cmd1);
 	ft_clean_array(cmd2);
 	return (0);
+	
 }
